c039f1b1cc14a8bb85e3d8e54ae5f66762176ee7
Michael Smith <michael.jt.smith@gmail.com>
2021-08-19 14:21:18 -0700

    Use logger

--- src/main/java/me/pseudoknight/CHStargate/CHStargate.java
+++ src/main/java/me/pseudoknight/CHStargate/CHStargate.java
@@ -3,9 +3,12 @@ package me.pseudoknight.CHStargate;
 import com.laytonsmith.PureUtilities.SimpleVersion;
 import com.laytonsmith.PureUtilities.Version;
 import com.laytonsmith.abstraction.Implementation;
+import com.laytonsmith.core.Static;
 import com.laytonsmith.core.extensions.AbstractExtension;
 import com.laytonsmith.core.extensions.MSExtension;
 import me.pseudoknight.CHStargate.abstraction.Stargate.Listener;
+
+import java.util.logging.Level;
 
 @MSExtension("CHStargate")
 public class CHStargate extends AbstractExtension {
@@ -18,9 +21,9 @@ public class CHStargate extends AbstractExtension {
 	public void onStartup() {
 		if(Implementation.GetServerType() == Implementation.Type.BUKKIT) {
 			Listener.register();
-			System.out.println("CHStargate " + getVersion() + " loaded.");
+			Static.getLogger().log(Level.INFO, "CHStargate " + getVersion() + " loaded.");
 		} else {
-			System.out.println("CHStargate not supported on this implementation.");
+			Static.getLogger().log(Level.INFO, "CHStargate not supported on this implementation.");
 		}
 	}
 
@@ -28,7 +31,7 @@ public class CHStargate extends AbstractExtension {
 	public void onShutdown() {
 		if(Implementation.GetServerType() == Implementation.Type.BUKKIT) {
 			Listener.unregister();
-			System.out.println("CHStargate " + getVersion() + " unloaded.");
+			Static.getLogger().log(Level.INFO, "CHStargate " + getVersion() + " unloaded.");
 		}
 	}
 
