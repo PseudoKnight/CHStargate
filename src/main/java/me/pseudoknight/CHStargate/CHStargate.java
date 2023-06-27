881aa545aa76fe100db90f6a4feb6e9abe4fec4d
Michael Smith <michael.jt.smith@gmail.com>
2023-06-27 01:18:23 -0700

    Release 3.1.0

--- src/main/java/me/pseudoknight/CHStargate/CHStargate.java
+++ src/main/java/me/pseudoknight/CHStargate/CHStargate.java
@@ -2,37 +2,28 @@ package me.pseudoknight.CHStargate;
 
 import com.laytonsmith.PureUtilities.SimpleVersion;
 import com.laytonsmith.PureUtilities.Version;
-import com.laytonsmith.abstraction.Implementation;
 import com.laytonsmith.core.Static;
 import com.laytonsmith.core.extensions.AbstractExtension;
 import com.laytonsmith.core.extensions.MSExtension;
 import me.pseudoknight.CHStargate.abstraction.Stargate.Listener;
 
-import java.util.logging.Level;
-
 @MSExtension("CHStargate")
 public class CHStargate extends AbstractExtension {
 
 	public Version getVersion() {
-		return new SimpleVersion(3,0,1);
+		return new SimpleVersion(3,1,0);
 	}
 
 	@Override
 	public void onStartup() {
-		if(Implementation.GetServerType() == Implementation.Type.BUKKIT) {
-			Listener.register();
-			Static.getLogger().log(Level.INFO, "CHStargate " + getVersion() + " loaded.");
-		} else {
-			Static.getLogger().log(Level.INFO, "CHStargate not supported on this implementation.");
-		}
+		Listener.register();
+		Static.getLogger().info("CHStargate " + getVersion() + " loaded.");
 	}
 
 	@Override
 	public void onShutdown() {
-		if(Implementation.GetServerType() == Implementation.Type.BUKKIT) {
-			Listener.unregister();
-			Static.getLogger().log(Level.INFO, "CHStargate " + getVersion() + " unloaded.");
-		}
+		Listener.unregister();
+		Static.getLogger().info("CHStargate " + getVersion() + " unloaded.");
 	}
 
 }
